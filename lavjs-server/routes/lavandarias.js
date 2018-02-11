dd20e22714b1be07b3f3021a9d50e95adbc760ec
Tiago Matias <tiago.matias92@gmail.com>
2018-02-11 12:48:54 +0000

    Lav feature API done

--- lavjs-server/routes/lavandarias.js
+++ lavjs-server/routes/lavandarias.js
@@ -11,10 +11,73 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
+router.get('/all', (req, res, next) => {
     res.status(200).json({
-        message: 'POST - lav'
+        message: 'List All'
     });
+});
+
+router.get('/:lavId', (req, res, next) => {
+    const id = req.params.lavId;
+
+    Lav.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("database");
+            if(doc)
+                res.status(200).json(doc);
+            else
+                res.status(500).json({message: 'No Id'});
+        })
+        .catch();
+});
+
+router.post('/new', (req, res, next) => {
+
+    const lav = new Lav ({
+        local: req.body.local,
+        name: req.body.nome,
+        liq_total: req.body.liq_total
+    });
+
+    lav
+        .save()
+        .then(result => {
+            console.log(Sucess);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    
+    res.status(201).json({
+        message: 'New Lav',
+        lav: lav
+    });
+});
+
+router.patch('/:lavId', (req, res, next) => {
+    const requestedId = req.params.lavId;
+
+    const updateOps = {};
+    for (const ops in req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    console.log(requestedId);
+    Lav.update({ _id: requestedId }, { $set: req.body })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Update - Done',
+                result: result
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
 });
 
 module.exports = router;
